998bc3c15e0ecf05fee8002dff6b0ccc56dcfb46
Parneet Singh Sarao <parneet1650.be20@chitkara.edu.in>
2023-03-25 18:58:13 +0530

    User, Tour, Auth: routes, models, functions

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors')
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const authRoute = require('./routes/auth');
+const tourRoute = require('./routes/tours');
+const userRoute = require('./routes/users');
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+const corsOptions = {
+    origin: true,
+    credentials: true
+}
+
+// connecting database
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('MongoDB database connected');
+}).catch((error) => {
+    console.log('MongoDB database connection failed', error);
+});
+
+// middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/tours', tourRoute);
+app.use('/api/v1/users', userRoute);
+
+// listening at PORT : 5000
+app.listen(PORT, () => {
+    console.log(`server runnin on ${PORT}\nhttp://localhost:${PORT}`);
+})
