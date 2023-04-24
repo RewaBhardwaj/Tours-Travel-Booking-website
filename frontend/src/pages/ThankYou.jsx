ee26fe7cb04946720b25541476af570c2bcf5f71
Manish <78153901+manish15walia@users.noreply.github.com>
2023-04-24 21:07:15 +0530

    Changes in ThankYou file

--- frontend/src/pages/ThankYou.jsx
+++ frontend/src/pages/ThankYou.jsx
@@ -14,7 +14,7 @@ const ThankYou = () => {
                 <span><i class="ri-checkbox-circle-line"></i></span>
                 <h1 className="mb-3 fw-semibold">Thank You</h1>
 
-                <h3 className="mb-4">your tour is booked.</h3>
+                <h3 className="mb-4">Your Tour is Booked.</h3>
 
 
                 <Button className="btn primary__btn w-25">
