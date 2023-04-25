362877a912c685453da11ffac202173f701722d6
Parneet Singh Sarao <parneet1650.be20@chitkara.edu.in>
2023-04-25 13:32:40 +0530

    Booking section integrated

--- frontend/src/components/Booking/Booking.jsx
+++ frontend/src/components/Booking/Booking.jsx
@@ -1,93 +1,133 @@
-import React, {useState} from 'react'
+import React, { useContext, useState } from 'react'
 import './booking.css'
 import { Form, FormGroup, ListGroup, ListGroupItem, Button } from 'reactstrap'
 import { useNavigate } from 'react-router-dom'
-const Booking = ( {tour, avgRating} ) => {
+import { AuthContext } from '../../Context/AuthContext'
+import { BASE_URL } from '../../utils/config'
+const Booking = ({ tour, avgRating }) => {
 
-    const {price, reviews} = tour
+    const { price, reviews, title, maxGroupSize } = tour
     const navigate = useNavigate()
-    const [credentials,setCredentials] = useState({
-        userId: '01',  //later it will be dynamic
-        userEmail: 'example@gmail.com',
+
+    const {user} = useContext(AuthContext);
+
+    const [booking, setBooking] = useState({
+        userId: user && user._id,
+        userEmail: user && user.email,
+        tourName: title,
         fullName: '',
         phone: '',
-        guestSize: 1,
+        guestSize: 0,
         bookAt: ''
     })
 
-    const handleChange = e=>{
-        setCredentials(prev=>({...prev, [e.target.id]:e.target.value}))
+    const handleChange = e => {
+        setBooking(prev => ({ ...prev, [e.target.id]: e.target.value }))
     }
-    
+
     const serviceFee = 10
-    const totalAmount = Number(price) * Number(credentials.guestSize) + Number(serviceFee)
+    const totalAmount = Number(price) * Number(booking.guestSize) + Number(serviceFee)
 
     //send data to the server
 
-    const handleClick = e=>{
+    const handleClick = async(e) => {
         e.preventDefault()
-        navigate("/thank-you")
+
+        if(booking.fullName === '' || booking.phone === '' || booking.guestSize === 0) {
+            return alert('Please fill the details');
+        }
+
+        if(booking.guestSize > maxGroupSize) {
+            return alert(`Guests are more than the required capacity\nMax Group Size is: ${maxGroupSize}`);
+        }
+        console.log(booking);
+
+        try {
+            if(!user || user === undefined || user === null) {
+                return alert('Please Sign in');
+            }
+
+            const res = await fetch(`${BASE_URL}/booking`, {
+                method: 'post',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                credentials: 'include',
+                body: JSON.stringify(booking)
+            });
+
+            const result = await res.json();
+            
+            if(!res.ok) {
+                return alert(result.message);
+            }
+
+            navigate("/thank-you");
+        } catch (error) {
+            alert(error.message);
+        }
+
     }
 
-  return (
-    <div className="booking">
-        <div className="booking__top d-flex align-items-center justify-content-between">
-            <h3>
-                ${price}
-                <span>
-                    /per person
+    return (
+        <div className="booking">
+            <div className="booking__top d-flex align-items-center justify-content-between">
+                <h3>
+                    ${price}
+                    <span>
+                        /per person
+                    </span>
+                </h3>
+                <span className="tour__rating d-flex align-items-center">
+                    <i className="ri-star-s-fill"></i>
+                    {avgRating === 0 ? null : avgRating} ({reviews?.length})
                 </span>
-            </h3>
-            <span className="tour__rating d-flex align-items-center">
-                <i class="ri-star-s-fill"></i>
-                {avgRating === 0 ? null : avgRating} ({reviews ?. length})
-            </span>
-        </div>
+            </div>
 
-        {/* booking form start */}
-        <div className="booking__form">
-            <h5>Information</h5>
-            <Form className="booking__info-form" onSubmit={handleClick}>
-                <FormGroup>
-                    <input type="text" placeholder="Full Name" id="fullName" required onChange={handleChange}/>
-                </FormGroup>
-                <FormGroup>
-                    <input type="number" placeholder="Phone" id="phone" required onChange={handleChange}/>
-                </FormGroup>
-                <FormGroup className="d-flex align-items-center gap-3">
-                    <input type="date" placeholder="" id="bookAt" required onChange={handleChange}/>
-                    <input type="number" placeholder="Guest" id="guestSize" required onChange={handleChange}/>
-                </FormGroup>
-            </Form>
-        </div>
-        {/* booking form end */}
+            {/* booking form start */}
+            <div className="booking__form">
+                <h5>Your Details</h5>
+                <Form className="booking__info-form" onSubmit={handleClick}>
+                    <FormGroup>
+                        <input type="text" placeholder="Full Name" id="fullName" onChange={handleChange} />
+                    </FormGroup>
+                    <FormGroup>
+                        <input type="number" placeholder="Phone" id="phone" onChange={handleChange} />
+                    </FormGroup>
+                    <FormGroup className="d-flex align-items-center gap-3">
+                        <input type="date" placeholder="" id="bookAt" onChange={handleChange} />
+                        <input type="number" placeholder="Guest" id="guestSize" onChange={handleChange} />
+                    </FormGroup>
+                </Form>
+            </div>
+            {/* booking form end */}
 
-        {/* booking bottom */}
-        <div className="booking__bottom">
-            <ListGroup>
-                <ListGroupItem className="border-0 px-0">
-                    <h5 className="d-flex align-items-center gap-1">${price} <i class="ri-close-line"></i> 1 person </h5>
-                    <span>
-                        ${price}
-                    </span>
-                </ListGroupItem>
-                <ListGroupItem className="border-0 px-0">
-                    <h5>Service charge</h5>
-                    <span>
-                        ${serviceFee}
-                    </span>
-                </ListGroupItem>
-                <ListGroupItem className="border-0 px-0 total">
-                    <h5>Total</h5>
-                    <span>
-                        ${totalAmount}
-                    </span>
-                </ListGroupItem>
-            </ListGroup>
-            <Button className="btn primary__btn w-100 mt-4" onClick={handleClick}> Book Now </Button>
+            {/* booking bottom */}
+            <div className="booking__bottom">
+                <ListGroup>
+                    <ListGroupItem className="border-0 px-0">
+                        <h5 className="d-flex align-items-center gap-1">${price} <i className="ri-close-line"></i> 1 person </h5>
+                        <span>
+                            ${price}
+                        </span>
+                    </ListGroupItem>
+                    <ListGroupItem className="border-0 px-0">
+                        <h5>Service charge</h5>
+                        <span>
+                            ${serviceFee}
+                        </span>
+                    </ListGroupItem>
+                    <ListGroupItem className="border-0 px-0 total">
+                        <h5>Total</h5>
+                        <span>
+                            ${totalAmount}
+                        </span>
+                    </ListGroupItem>
+                </ListGroup>
+                <Button className="btn primary__btn w-100 mt-4" onClick={handleClick}> Book Now </Button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Booking
--- frontend/src/pages/TourDetails.jsx
+++ frontend/src/pages/TourDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import "../styles/tour-details.css";
 import { Container, Row, Col, Form, ListGroup } from "reactstrap";
 import { useParams } from "react-router-dom";
@@ -8,11 +8,13 @@ import Booking from "../components/Booking/Booking";
 import Newsletter from "../shared/Newsletter";
 import useFetch from "../hooks/useFetch";
 import { BASE_URL } from "../utils/config";
+import { AuthContext } from "../Context/AuthContext";
 
 const TourDetails = () => {
   const { id } = useParams();
   const reviewMsgRef = useRef('');
-  const [tourRating, setTourRating] = useState(null);
+  const [tourRating, setTourRating] = useState(5);
+  const {user} = useContext(AuthContext);
 
   // fetching data from database
   const { data: tour, loading, error } = useFetch(`${BASE_URL}/tours/${id}`);
@@ -38,11 +40,38 @@ const TourDetails = () => {
 
   //submit request to the server
 
-  const handleSubmit = e => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const reviewText = reviewMsgRef.current.value
-    alert(`${reviewText}, ${tourRating}`)
-    //later will call aur api
+    const reviewText = reviewMsgRef.current.value;
+    
+    try {
+      if(!user || user === undefined || user === null) {
+        return alert('Please sign in');
+      }
+
+      const reviewObj = {
+        username: user?.username,
+        reviewText,
+        rating: tourRating
+      }
+
+      const res = await fetch(`${BASE_URL}/review/${id}`, {
+        method: 'post',
+        headers: {
+          'content-type': 'application/json'
+        },
+        credentials: 'include',
+        body: JSON.stringify(reviewObj)
+      });
+
+      const result = await res.json();
+      if(!res.ok) {
+        return alert(result.message);
+      }
+      alert('Review Submitted');
+    } catch (err) {
+      alert(err.message);
+    }
   }
 
   useEffect(() => {
@@ -80,7 +109,7 @@ const TourDetails = () => {
              align-items-center gap-1"
                       >
                         <i
-                          class="ri-star-fill"
+                          className="ri-star-fill"
                           style={{ color: "var(--secondary-color" }}
                         ></i>
                         {avgRating === 0 ? null : avgRating}
@@ -92,21 +121,21 @@ const TourDetails = () => {
                       </span>
 
                       <span>
-                        <i class="ri-map-pin-user-fill"></i>{address}
+                        <i className="ri-map-pin-user-fill"></i>{address}
                       </span>
                     </div>
 
                     <div className="tour__extra-details">
                       <span>
-                        <i class="ri-map-pin-2-line"></i>{city}</span>
+                        <i className="ri-map-pin-2-line"></i>{city}</span>
                       <span>
-                        <i class="ri-money-dollar-circle-line"></i>${price} /
+                        <i className="ri-money-dollar-circle-line"></i>${price} /
                         per person </span>
                       <span>
-                        <i class="ri-map-pin-time-line"></i> {distance} k/m
+                        <i className="ri-map-pin-time-line"></i> {distance} k/m
                       </span>
                       <span>
-                        <i class="ri-group-line"></i>
+                        <i className="ri-group-line"></i>
                         {maxGroupSize} people
                       </span>
                     </div>
@@ -121,19 +150,13 @@ const TourDetails = () => {
                     <Form onSubmit={handleSubmit}>
                       <div className="d-flex align-items-center gap-3 mb-4
                     rating__group">
-                        <span onClick={() => setTourRating(1)}>
-                          1 <i class="ri-star-s-fill"></i>
-                        </span>
-                        <span onClick={() => setTourRating(2)}>
-                          2 <i class="ri-star-s-fill"></i>
-                        </span>
-                        <span onClick={() => setTourRating(3)}>
-                          3 <i class="ri-star-s-fill"></i></span>
-                        <span onClick={() => setTourRating(4)}>
-                          4 <i class="ri-star-s-fill"></i></span>
-                        <span onClick={() => setTourRating(5)}>
-                          5 <i class="ri-star-s-fill"></i></span>
-
+                        {
+                          [...Array(5)].map((item, idx) =>
+                            <span key={idx} onClick={() => setTourRating(idx+1)}>
+                              {idx+1} <i className="ri-star-s-fill"></i>
+                            </span>
+                          )
+                        }
                       </div>
                       <div className="review__input">
                         <input type="text" ref={reviewMsgRef} placeholder="share your thoughts" required />
@@ -147,26 +170,26 @@ const TourDetails = () => {
 
                     <ListGroup className="user__reviews">
                       {
-                        reviews?.map(reviews => (
-                          <div className="review__item" key={reviews._id}>
+                        reviews?.map(review => (
+                          <div className="review__item" key={review._id}>
                             <img src={avatar} alt="" />
 
                             <div className="w-100">
                               <div className="d-flex align-items-center
                             justify-content-between">
                                 <div>
-                                  <h5>muhib</h5>
-                                  <p>{new Date('01-18-2023').toLocaleDateString(
+                                  <h5>{review.username}</h5>
+                                  <p>{new Date(review.createdAt).toLocaleDateString(
                                     "en-US",
                                     options
                                   )}
                                   </p>
                                 </div>
                                 <span className="d-flex align-items-center">
-                                  5<i class="ri-star-s-fill"></i>
+                                  {review.rating}<i className="ri-star-s-fill"></i>
                                 </span>
                               </div>
-                              <h6>Amazing tour</h6>
+                              <h6>{review.reviewText}</h6>
                             </div>
                           </div>
                         ))}
@@ -175,9 +198,12 @@ const TourDetails = () => {
                   {/*-------tour reviews section end------- */}
                 </div>
               </Col>
+
+              {/* Booking Section start here */}
               <Col lg='4'>
                 <Booking tour={tour} avgRating={avgRating} />
               </Col>
+              {/* Booking Section start here */}
             </Row>
           }
         </Container>
